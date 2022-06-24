4a8ed3ff99c59687a56753d824d301f96eddc747
Ryan Sweeney <ryan_sweeney@outlook.ie>
2022-06-24 17:10:07 +0100

    adding dist

--- src/components/SocialMedia.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div class="text-center">
-		<h2>In the meantime check these places out</h2>
-		<h3><a href="https://www.linkedin.com/in/ryanairth-sweeney/">LinkedIn</a></h3>
-		<h3><a href="https://github.com/RyanSweeney987">Github</a></h3>
-		<h3><a href="https://www.youtube.com/c/RyanAirth987/featured">YouTube</a></h3>
-		<h3><a href="https://www.instagram.com/sweeneygamedev/?hl=en">Instagram</a></h3>
-	</div>
-</template>
