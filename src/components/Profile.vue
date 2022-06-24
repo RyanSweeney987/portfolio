881841a20ef5fcbcc60565c3280d2e3c7dde397c
Ryan Sweeney <ryan_sweeney@outlook.ie>
2022-06-24 16:51:37 +0100

    Testing the use of Vue

---
+++ src/components/Profile.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+	<div class="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+		This is a test
+	</div>
+</template>
+
+<style scoped>
+
+</style>
